87789ab02d8e6fa98615fd0c69d142dab88d199b
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-04-07 01:41:49 +0000

    Add forms and template to views

--- Reviews/views.py
+++ Reviews/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .form import Reviewform
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
-    return render(request, "reviews/review.html")
+    if request.method == "POST":
+        form = Reviewform()
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+
+    else:   
+        form = Reviewform() 
+
+    return render(request, "reviews/review.html", {
+        "form": form
+    })
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
