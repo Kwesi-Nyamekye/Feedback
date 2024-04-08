6c3cb01089e22f4974874ff94ce1d02a08a32a10
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-04-08 23:49:58 +0000

    Add models and forms in views

--- Reviews/views.py
+++ Reviews/views.py
@@ -1,23 +1,40 @@
+from typing import Any
 from django.shortcuts import render
-from .form import Reviewform
+from .form import ReviewForm
 from django.http import HttpResponseRedirect
+from django.views import View
+from django.views.generic.base import TemplateView
+from .models import Review
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-def review(request):
-    if request.method == "POST":
-        form = Reviewform()
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm() 
+
+        return render(request, "reviews/review.html", {
+           "form": form })
+    
+    def post(self, request):
+        form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            form.save()
             return HttpResponseRedirect("/thank-you")
+        
+        return render(request, "reviews/review.html", {
+        "form": form })
 
-    else:   
-        form = Reviewform() 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+    
+class ReviewListView(ListView):
+    template_name="reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
-
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
