6fe0280b6e9a6252b43619dc6bb6dd4406019637
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-04-08 23:50:33 +0000

    Configure url path for various views

--- Reviews/urls.py
+++ Reviews/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
 from . import views
 
+
 urlpatterns = [
-    path("", views.review)
+    path("", views.ReviewView.as_view()),
+    path("thank-you", views.ThankYouView.as_view()),
+    path("review", views.ReviewListView.as_view()),
+    path("reviews/<int:pk>", views.SingleReviewView.as_view() )
 ]
