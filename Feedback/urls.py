46c11b6d98723adb7222fb2c26403fa3c3cde6d5
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-04-09 23:50:42 +0000

    Configure url path for profiles app

--- Feedback/urls.py
+++ Feedback/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("Reviews.urls"))
+    path("", include("Reviews.urls")),
+    path("profiles/", include("profiles.urls"))
 ]
